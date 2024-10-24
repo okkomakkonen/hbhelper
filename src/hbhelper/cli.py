db017abf60f39ec8af963e6d64981b353a0a167f
Okko Makkonen <okko.makkonen@proton.me>
2024-10-24 22:31:03 +0300

    Change backends to classes

--- src/hbhelper/cli.py
+++ src/hbhelper/cli.py
@@ -3,7 +3,7 @@ import os
 
 import click
 
-from .converters import s_pankki_to_dataframe, splitwise_to_dataframe, is_s_pankki_format, is_splitwise_format
+from .converters import converter_types, Converter
 
 
 @click.group()
@@ -22,84 +22,35 @@ def cli():
 def convert_command(filename: str):
     """Convert file to HomeBank CSV format"""
 
-    possible_formats: list[str] = []
+    possible_converters: list[Converter] = []
 
-    if is_s_pankki_format(filename):
-        possible_formats.append("S-Pankki")
-    
-    if is_splitwise_format(filename):
-        possible_formats.append("Splitwise")
+    for converter_type in converter_types:
+        converter = converter_type(filename)
+        if converter.parse():
+            possible_converters.append(converter)
 
-    if len(possible_formats) != 1:
+    if len(possible_converters) != 1:
         click.echo("Could not figure out file format")
         return 1
 
-    format = possible_formats[0]
+    converter = possible_converters[0]
 
-    if format == "S-Pankki":
-        df = s_pankki_to_dataframe(filename)
-    elif format == "Splitwise":
-        df = splitwise_to_dataframe(filename)
-    else:
-        return 1
+    df = converter.convert()
 
-    today = datetime.today()
-    today_str = today.strftime("%Y-%m-%d")
+    today = datetime.today().strftime("%Y-%m-%d")
 
-    df["tags"] += f"hbhelper-{today_str} "
+    df.loc[:, "tags"] += f"hbhelper-{today} "
 
     dirname = os.path.dirname(os.path.realpath(filename))
 
-    out_filename = os.path.join(dirname, f"hbhelper-{today_str}-{filename}")
+    # TODO: fix this
+    out_filename = f"hbhelper-{today}-{filename}"
+    out_path = os.path.join(dirname, out_filename)
 
-    df.to_csv(out_filename, sep=";", index=False)
+    df.to_csv(out_path, sep=";", index=False)
 
-    click.echo(f"Converted {filename} using {format} and wrote to file {out_filename}")
+    click.echo(
+        f"Converted {filename} using {converter.name} and wrote to file {out_filename}"
+    )
 
     return 0
-
-
-@cli.command("s-pankki")
-@click.argument(
-    "filename", nargs=1, type=click.Path(exists=True, readable=True, dir_okay=False)
-)
-def s_pankki_command(filename: str):
-    """Import from S-Pankki (DEPRECATED)"""
-
-    click.echo(f"Transforming {filename} to Homebank CSV format")
-
-    df = s_pankki_to_dataframe(filename)
-
-    today = datetime.today()
-
-    # 2024-10-20-hbhelper-s-pankki.csv
-    filename = today.strftime("%Y-%m-%d") + "-hbhelper-s-pankki.csv"
-
-    df.to_csv(filename, sep=";", index=False)
-
-    click.echo(f"Wrote final CSV to {filename}")
-
-    return
-
-
-@cli.command("splitwise")
-@click.argument(
-    "filename", nargs=1, type=click.Path(exists=True, readable=True, dir_okay=False)
-)
-def splitwise_command(filename: str):
-    """Import from Splitwise (DEPRECATED)"""
-
-    click.echo(f"Transforming {filename} to Homebank CSV format")
-
-    df = splitwise_to_dataframe(filename)
-
-    today = datetime.today()
-
-    # 2024-10-20-hbhelper-splitwise.csv
-    filename = today.strftime("%Y-%m-%d") + "-hbhelper-splitwise.csv"
-
-    df.to_csv(filename, sep=";", index=False)
-
-    click.echo(f"Wrote final CSV to {filename}")
-
-    return
--- src/hbhelper/converters.py
+++ src/hbhelper/converters.py
@@ -1,143 +1,146 @@
+"""
+This file constructs converters to convert data from a bank or other institution
+to the format read by HomeBank.
+
+HomeBank expects a semicolon separated CSV file with the columns
+    [date, payment, number, payee, memo, amount, category, tags]
+"""
+
 import pandas as pd
 
-# HomeBank expected format
-# date, payment, number, payee, memo, amount, category, tags
+
+class Converter:
+    name = "default"
+
+    def __init__(self, filename: str) -> None:
+        self._filename = filename
+        self._df: pd.DataFrame | None = None
+
+    def parse(self) -> bool:
+        return NotImplemented
+
+    def convert(self) -> pd.DataFrame:
+        return NotImplemented
 
 
-def is_s_pankki_format(filename: str) -> bool:
-    # Read file to pd.DataFrame
-    # S-Pankki gives csv file with delimiter=";", decimal="," and columns:
-    # 'Kirjauspäivä', 'Maksupäivä', 'Summa', 'Tapahtumalaji', 'Maksaja', 'Saajan nimi',
-    # 'Saajan tilinumero', 'Saajan BIC-tunnus', 'Viitenumero', 'Viesti', 'Arkistointitunnus'
-    try:
-        df = pd.read_csv(filename, delimiter=";", decimal=",", quotechar="'")
-    except pd.errors.ParserError:
-        return False
+class SPankkiConverter(Converter):
+    name = "s-pankki"
 
-    # Verify that the file is in the correct format
-    EXPECTED_COLUMNS = [
-        "Kirjauspäivä",
-        "Maksupäivä",
-        "Summa",
-        "Tapahtumalaji",
-        "Maksaja",
-        "Saajan nimi",
-        "Saajan tilinumero",
-        "Saajan BIC-tunnus",
-        "Viitenumero",
-        "Viesti",
-        "Arkistointitunnus",
-    ]
-    columns = list(df.columns)
-    if len(columns) != 11 or EXPECTED_COLUMNS != columns:
-        return False
+    def parse(self) -> bool:
+        # Read file to pd.DataFrame
+        # S-Pankki gives csv file with delimiter=";", decimal=",", quotechar="'" and columns:
+        # 'Kirjauspäivä', 'Maksupäivä', 'Summa', 'Tapahtumalaji', 'Maksaja', 'Saajan nimi',
+        # 'Saajan tilinumero', 'Saajan BIC-tunnus', 'Viitenumero', 'Viesti', 'Arkistointitunnus'
+        try:
+            df = pd.read_csv(self._filename, delimiter=";", decimal=",", quotechar="'")
+        except pd.errors.ParserError:
+            return False
 
-    return True
+        # Verify that the file is in the correct format
+        EXPECTED_COLUMNS = [
+            "Kirjauspäivä",
+            "Maksupäivä",
+            "Summa",
+            "Tapahtumalaji",
+            "Maksaja",
+            "Saajan nimi",
+            "Saajan tilinumero",
+            "Saajan BIC-tunnus",
+            "Viitenumero",
+            "Viesti",
+            "Arkistointitunnus",
+        ]
+        columns = list(df.columns)
+        if len(columns) != 11 or EXPECTED_COLUMNS != columns:
+            return False
 
+        self._df = df
 
-def s_pankki_to_dataframe(filename: str) -> pd.DataFrame:
-    # Read file to pd.DataFrame
+        return True
 
-    # S-Pankki gives csv file with delimiter=";", decimal=",", quotechar="'" and columns:
-    # 'Kirjauspäivä', 'Maksupäivä', 'Summa', 'Tapahtumalaji', 'Maksaja', 'Saajan nimi',
-    # 'Saajan tilinumero', 'Saajan BIC-tunnus', 'Viitenumero', 'Viesti', 'Arkistointitunnus'
-    df = pd.read_csv(filename, delimiter=";", decimal=",", quotechar="'")
+    def convert(self) -> pd.DataFrame:
+        df = self._df
 
-    # Verify that the file is in the correct format
-    EXPECTED_COLUMNS = [
-        "Kirjauspäivä",
-        "Maksupäivä",
-        "Summa",
-        "Tapahtumalaji",
-        "Maksaja",
-        "Saajan nimi",
-        "Saajan tilinumero",
-        "Saajan BIC-tunnus",
-        "Viitenumero",
-        "Viesti",
-        "Arkistointitunnus",
-    ]
-    columns = list(df.columns)
-    if len(columns) != 11 or EXPECTED_COLUMNS != columns:
-        raise Exception("Unexpected columns")
+        if df is None:
+            raise Exception("incorrect format")
 
-    # Do some processing on the data
-    df.loc[df["Tapahtumalaji"] == "KORTTIOSTO", "Viesti"] = ""
-    df.loc[df["Viesti"] == "-", "Viesti"] = ""
+        # Do some processing on the data
+        df.loc[df["Tapahtumalaji"] == "KORTTIOSTO", "Viesti"] = ""
+        df.loc[df["Viesti"] == "-", "Viesti"] = ""
 
-    # Add new columns
-    df["date"] = pd.to_datetime(df["Maksupäivä"], format="%d.%m.%Y")
-    df["payment"] = ""
-    df["number"] = ""
-    df["payee"] = ""
-    df["memo"] = df["Viesti"]
-    df["amount"] = df["Summa"]
-    df["category"] = ""
-    df["tags"] = ""
+        # Add new columns
+        df["date"] = pd.to_datetime(df["Maksupäivä"], format="%d.%m.%Y")
+        df["payment"] = ""
+        df["number"] = ""
+        df["payee"] = ""
+        df["memo"] = df["Viesti"]
+        df["amount"] = df["Summa"]
+        df["category"] = ""
+        df["tags"] = ""
 
-    # Add payee depending on the sign of the transaction
-    df.loc[df["Summa"] > 0, "payee"] = df["Maksaja"]
-    df.loc[df["Summa"] <= 0, "payee"] = df["Saajan nimi"]
+        # Add payee depending on the sign of the transaction
+        df.loc[df["Summa"] > 0, "payee"] = df["Maksaja"]
+        df.loc[df["Summa"] <= 0, "payee"] = df["Saajan nimi"]
 
-    # Remove unnecessary columns
-    df = df[
-        ["date", "payment", "number", "payee", "memo", "amount", "category", "tags"]
-    ]
+        # Remove unnecessary columns
+        df = df[
+            ["date", "payment", "number", "payee", "memo", "amount", "category", "tags"]
+        ]
 
-    return df
-
-def is_splitwise_format(filename: str) -> pd.DataFrame:
-    # Read file to pd.DataFrame
-    # Splitwise gives csv file with delimiter=",", decimal=".",
-    # first two rows and the last row containing garbage, and columns:
-    # 'Date', 'Description', 'Category', 'Cost', 'Currency', your_name, other_name
-    try:
-        df = pd.read_csv(filename, delimiter=",", decimal=".", skiprows=2)
-        df = df[:-1]
-    except pd.errors.ParserError:
-        return False
-
-    # Verify that the file is in the correct format
-    columns = list(df.columns)
-    EXPECTED_COLUMNS = ["Date", "Description", "Category", "Cost", "Currency"]
-    if len(columns) != 7 or EXPECTED_COLUMNS != columns[:5]:
-        return False
-    
-    return True
+        return df
 
 
-def splitwise_to_dataframe(filename: str) -> pd.DataFrame:
-    # Read file to pd.DataFrame
-    # Splitwise gives csv file with delimiter=",", decimal=".",
-    # first two rows and the last row containing garbage, and columns:
-    # 'Date', 'Description', 'Category', 'Cost', 'Currency', your_name, other_name
-    df = pd.read_csv(filename, delimiter=",", decimal=".", skiprows=2)
-    df = df[:-1]
+class SplitwiseConverter(Converter):
+    name = "splitwise"
 
-    # Verify that the file is in the correct format
-    columns = list(df.columns)
-    EXPECTED_COLUMNS = ["Date", "Description", "Category", "Cost", "Currency"]
-    if len(columns) != 7 or EXPECTED_COLUMNS != columns[:5]:
-        raise Exception("Unexpected columns")
+    def parse(self) -> bool:
+        # Read file to pd.DataFrame
+        # Splitwise gives csv file with delimiter=",", decimal=".",
+        # first two rows and the last row containing garbage, and columns:
+        # 'Date', 'Description', 'Category', 'Cost', 'Currency', your_name, other_name
+        try:
+            df = pd.read_csv(self._filename, delimiter=",", decimal=".", skiprows=2)
+            df = df[:-1]
+        except pd.errors.ParserError:
+            return False
 
-    # The column your_name contains the amount for you
-    # TODO: this is wrong, the columns are in alphabetical order
-    your_name = df.columns[-2]
-    other_name = df.columns[-1]
+        # Verify that the file is in the correct format
+        columns = list(df.columns)
+        EXPECTED_COLUMNS = ["Date", "Description", "Category", "Cost", "Currency"]
+        if len(columns) != 7 or EXPECTED_COLUMNS != columns[:5]:
+            return False
 
-    # Add new columns
-    df["date"] = pd.to_datetime(df["Date"])
-    df["payment"] = ""
-    df["number"] = ""
-    df["payee"] = other_name
-    df["memo"] = df["Description"]
-    df["amount"] = df[your_name]
-    df["category"] = ""
-    df["tags"] = ""
+        self._df = df
 
-    # Remove unnecessary columns
-    df = df[
-        ["date", "payment", "number", "payee", "memo", "amount", "category", "tags"]
-    ]
+        return True
 
-    return df
+    def convert(self) -> pd.DataFrame:
+        df = self._df
+
+        if df is None:
+            raise Exception("incorrect format")
+
+        # The column your_name contains the amount for you
+        # TODO: this is wrong, the columns are in alphabetical order
+        your_name = df.columns[-2]
+        other_name = df.columns[-1]
+
+        # Add new columns
+        df["date"] = pd.to_datetime(df["Date"])
+        df["payment"] = ""
+        df["number"] = ""
+        df["payee"] = other_name
+        df["memo"] = df["Description"]
+        df["amount"] = df[your_name]
+        df["category"] = ""
+        df["tags"] = ""
+
+        # Remove unnecessary columns
+        df = df[
+            ["date", "payment", "number", "payee", "memo", "amount", "category", "tags"]
+        ]
+
+        return df
+
+
+converter_types = [SPankkiConverter, SplitwiseConverter]
